a714abc4747cb19a292ed2b938c2aadce40cb945
voidBit <saccullo.francesco@gmail.com>
2022-11-10 18:34:02 +0100

    Stilizzati item

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 
+
 const ItemComponent = ({item}) => {
-    console.log(item.image_url)
+
     return (
-        <View>
-            <Image  />
+        <View style={styles.container}>
+            <Image style={styles.image} source={{uri: item.image_url}} />
+            <View style={styles.infoContainer}>
+                <Text style={styles.title}>{item.name}</Text>
+                <View style={styles.infoMeta}>
+                    <Text style={styles.label}>{item.rating} Stelle,</Text> 
+                    <Text style={styles.label}> {item.review_count} Recensioni</Text>
+                </View>
+            </View>
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        marginRight: 20
+    },
+    image: {
+        width: 200,
+        height: 130
+    },
+    infoContainer: {
+        marginTop: 10
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: '600',
+        lineHeight: 22
+    },
+    infoMeta: {
+       flexDirection: 'row',
+    },
+    label: {
+        fontWeight: '400', 
+        fontSize: 12,
+        lineHeight: 16,
+        color: 'rgb(60, 60, 67,)' ,
+        opacity: 0.6
+    }
+});
 
 export default ItemComponent;
